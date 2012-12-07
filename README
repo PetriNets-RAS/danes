5a9f9f4a500aa592dd902ce85f33f4c5b8a16ae1
Michal Kaukic <kaukic@ulabserv.fri.uniza.sk>
2012-12-07 14:28:27 +0100

    Initial commit

---
+++ README
@@ -0,0 +1,2 @@
+Tu bude projekt pre editor, len vyvijany softver. 
+Nemiesat ine veci!!! 
---
+++ fib.py
@@ -0,0 +1,14 @@
+# somarinka, len na vyskusanie pridania suboru
+
+def fib(n):
+  """fib(n) - vrati n-ty clen Fib. postupnosti
+  """
+  
+  assert((type(n) is int) and (n>0)),"Indexy su nezaporne cele" 
+  if n in [0,1]:
+    return n
+  p,a=0,1
+  for k in range(2,n+1):
+    a,p=a+p,a
+  return a  
+        
